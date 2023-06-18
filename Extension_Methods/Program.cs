c34bddcc90814fd815a712d366b624086732b917
Jonathan Carlos Moura da Silva <jonathan.carllos1994@gmail.com>
2023-06-17 21:47:03 -0300

    Adicionar arquivos de projeto.

---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿using Extension_Methods.Extension;
+
+namespace Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
